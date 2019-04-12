3c8f6006323b704d76c3b79327e3900863e9f897
m.dinh10k <m.dinh10k@gmail.com>
2019-04-12 11:04:32 +0700

    add sentence

--- tools/test.py
+++ tools/test.py
@@ -1,10 +1,15 @@
-import wordnet
+from ais import wordnet
+from ais import sentence
+test_data = ['phòng','đảng','chính quyền','làm chủ','ban','đảng viên']
 
-test_data = ['đảng','chính quyền','làm chủ','ban','đảng viên']
+sample_wordnet = wordnet.Wordnet('wordnet10-4-2019.xlsx')
 
-sample = wordnet.Wordnet('wordnet10-4-2019.xlsx')
-
+#get synset
 for each in test_data:
     print(sample.get_ez_synsets_as_row(each))
 
+tag_list = ['n','v','a','e']
+sentence = 'thế lực thù địch có những âm mưu gì'
 
+s = sentence.Sentence(sentence,tag_list)
+print(s.get_similar_sentences_as_list(sample))
